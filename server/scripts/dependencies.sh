ac412763f315c4b544ae6ec26b5f5ad8a8bb9c1b
Patiopatio <buczkowska.p@gmail.com>
2019-04-05 17:01:25 +0200

    separated server

--- server/scripts/dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# using venv kernel
-pip install ipykernel;
-ipython kernel install --user --name=venv;
-# dependencies HERE
-pip install tweepy;
-pip install textblob;
-pip install flask;
-pip install flask-cors;
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source server/scripts/virtualenv.sh;
-./server/scripts/dependencies.sh;
-./server/scripts/server.sh;
